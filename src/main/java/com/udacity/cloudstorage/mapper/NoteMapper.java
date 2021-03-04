009c3f595669c713c480a24abb7f14ada283ef97
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-03-04 01:17:45 +0100

    fix error in NoteMapper

--- src/main/java/com/udacity/cloudstorage/mapper/NoteMapper.java
+++ src/main/java/com/udacity/cloudstorage/mapper/NoteMapper.java
@@ -11,19 +11,16 @@ public interface NoteMapper {
     @Select("SELECT * FROM NOTES WHERE userid = #{userId}")
     List<Note> getNotesByUserId(Integer userId);
 
-    @Update("UPDATE NOTES SET notetitle = #{noteTitle}, notedescription = #{noteDescription} WHERE noteid = #{noteId}")
-    void updateNote(Note note);
-
-    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
+    @Insert("INSERT INTO NOTES (noteTitle, noteDescription, userid) VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
     int saveNote(Note note);
 
-    @Delete("DELETE FROM NOTES WHERE noteid = #{noteId}")
+    @Delete("DELETE FROM NOTES WHERE noteId = #{noteId}")
     void deleteNoteByNoteId(Integer noteId);
 
-    @Select("SELECT * FROM NOTES WHERE noteid = #{noteId}")
+    @Select("SELECT * FROM NOTES WHERE noteId = #{noteId}")
     Note getNote(Integer noteId);
 
-    @Update("UPDATE NOTES SET notetitle = #{title}, notedescription = #{description} WHERE noteid = #{noteId}")
-    void updateNote(Integer noteId, String title, String description);
+    @Update("UPDATE NOTES SET noteTitle = #{noteTitle}, noteDescription = #{noteDescription} WHERE noteId = #{noteId}")
+    void updateNote(Integer noteId, String noteTitle, String description);
 }
