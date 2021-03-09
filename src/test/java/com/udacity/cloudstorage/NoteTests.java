6ac343ff34853af2cc148f77ded2e0b23b333ab2
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-03-09 19:26:08 +0100

    update delete notes test with Junit and Selenium

--- src/test/java/com/udacity/cloudstorage/NoteTests.java
+++ src/test/java/com/udacity/cloudstorage/NoteTests.java
@@ -1,5 +1,6 @@
 package com.udacity.cloudstorage;
 
+import com.udacity.cloudstorage.entity.Credential;
 import com.udacity.cloudstorage.entity.Note;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
@@ -14,6 +15,8 @@ public class NoteTests {
     public static final String USERNAME = "khalil";
     public static final String NOTE_TITLE = "NOTHING IMPORTANT";
     private static final String NOTE_DESCRIPTION = "NOTHING";
+    public static final String NOTE_TITLE_2 = "NOTHING IMPORTANT 2";
+    private static final String NOTE_DESCRIPTION_2 = "NOTHING 2";
     public static final String PASSWORD = "khalef";
     protected WebDriver driver;
 
@@ -75,6 +78,58 @@ public class NoteTests {
         Note note = homePage.getFirstNote();
         Assertions.assertEquals(NOTE_TITLE, note.getNoteTitle());
         Assertions.assertEquals(NOTE_DESCRIPTION, note.getNoteDescription());
+
+    }
+
+
+    @Test
+    public void testCredentialModificationAndDelete() {
+        driver.get("http://localhost:" + this.port + "/signup");
+        SignupPage signupPage = new SignupPage(driver);
+        signupPage.setFirstName(USERNAME);
+        signupPage.setLastName("khalef");
+        signupPage.setUserName(USERNAME);
+        signupPage.setPassword(PASSWORD);
+        signupPage.signUp();
+        driver.get("http://localhost:" + this.port + "/login");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.setUserName(USERNAME);
+        loginPage.setPassword(PASSWORD);
+        loginPage.login();
+
+        HomePage homePage = new HomePage(driver);
+        homePage.navToNotesTab();
+        homePage.addNewNote();
+        homePage.setNoteTitle(NOTE_TITLE);
+        homePage.setNoteDescription(NOTE_DESCRIPTION);
+        homePage.saveNoteChanges();
+        ResultPage resultPage = new ResultPage(driver);
+        resultPage.clickOk();
+
+        homePage.navToNotesTab();
+        Note note = homePage.getFirstNote();
+        homePage.editNote();
+        homePage.setNoteTitle(NOTE_TITLE_2);
+        homePage.setNoteDescription(NOTE_DESCRIPTION_2);
+        homePage.saveNoteChanges();
+
+        ResultPage result = new ResultPage(driver);
+        result.clickOk();
+
+        homePage.navToNotesTab();
+        Note modifiedNote = homePage.getFirstNote();
+
+        Assertions.assertEquals(NOTE_TITLE_2, modifiedNote.getNoteTitle());
+        Assertions.assertEquals(NOTE_DESCRIPTION_2, modifiedNote.getNoteDescription());
+
+        Assertions.assertNotEquals(NOTE_TITLE_2, note.getNoteTitle());
+        Assertions.assertNotEquals(NOTE_DESCRIPTION_2, note.getNoteDescription());
+
+        homePage.deleteNote();
+        result.clickOk();
+        homePage.logout();
+        Assertions.assertEquals("Login", driver.getTitle());
 
     }
 
